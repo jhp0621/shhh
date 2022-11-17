05e1ca4e23801c87fd624e13353aacaecfc36004
Ji Park <jhp0621@gmail.com>
2022-11-16 18:15:18 -0600

    mute and unmute slack based on slack user presence status

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { App, ExpressReceiver } = require("@slack/bolt");
 require("dotenv").config();
+const bodyParser = require("body-parser");
+const { SlackEventType, SlackUserPresenceStatus } = require("./config");
 
 // Create a Bolt Receiver
 const receiver = new ExpressReceiver({
@@ -11,6 +13,82 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   receiver,
 });
+
+/* to-do:
+ - post message to app channel when snooze is on/off?
+ - if snooze is already on when the user joins a meeting, do not overwrite it and do not unset when they leave meeting?
+*/
+
+// Slack interactions are methods on app
+app.client.auth.test({ token: process.env.SLACK_USER_TOKEN }).then((res) => {
+  // Find and store the slack app user's email
+  const userId = res.user_id;
+  app.client.users
+    .info({ token: process.env.SLACK_USER_TOKEN, user: userId })
+    .then((res) => {
+      receiver.app.locals.userEmail = res.user.profile.email;
+    });
+});
+
+// Other web requests are methods on receiver.router
+receiver.router.post(
+  "/",
+  bodyParser.raw({ type: "application/json" }),
+  async (req, res) => {
+    let event;
+    try {
+      event = JSON.parse(req.body);
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    if (event.event === SlackEventType.USER.PRESENCE_STATUS_UPDATED) {
+      const { presence_status: currentPresenceStatus, email: currentEmail } =
+        event.payload.object;
+
+      if (
+        currentEmail === receiver.app.locals.userEmail &&
+        req.headers.authorization === process.env.ZOOM_VERIFICATION_TOKEN
+      ) {
+        if (currentPresenceStatus === SlackUserPresenceStatus.IN_MEETING) {
+          console.log(`${currentEmail} joined a meeting`);
+          // Store inMeeting: true in express app locals object so it persist within the application
+          receiver.app.locals.inMeeting = true;
+          app.client.dnd
+            .setSnooze({
+              token: process.env.SLACK_USER_TOKEN,
+              // is_indefinite: true, // not working
+              num_minutes: 120,
+            })
+            .then((res) =>
+              console.log(
+                `Set user slack snooze: ${res.snooze_enabled ? "✅" : "⚠️"}`
+              )
+            );
+        } else if (
+          (currentPresenceStatus === SlackUserPresenceStatus.AVAILABLE ||
+            currentPresenceStatus === SlackUserPresenceStatus.OFFLINE ||
+            currentPresenceStatus ===
+              SlackUserPresenceStatus.IN_CALENDAR_EVENT) &&
+          // Check if the user was in a meeting before
+          receiver.app.locals.inMeeting
+        ) {
+          console.log(`${currentEmail} left a meeting`);
+          receiver.app.locals.inMeeting = false;
+          app.client.dnd
+            .endSnooze({
+              token: process.env.SLACK_USER_TOKEN,
+            })
+            .then((res) =>
+              console.log(
+                `End user slack snooze: ${res.snooze_enabled ? "⚠️" : "✅"}`
+              )
+            );
+        }
+      }
+    }
+  }
+);
 
 (async () => {
   const port = 3000;
---
+++ config.js
@@ -0,0 +1,90 @@
+const SlackEventType = {
+  MEETING: {
+    PARTICIPANT_JOINED: "meeting.participant_joined",
+    PARTICIPANT_LEFT: "meeting.participant_left",
+  },
+  USER: {
+    PRESENCE_STATUS_UPDATED: "user.presence_status_updated",
+  },
+};
+
+const SlackUserPresenceStatus = {
+  AVAILABLE: "Available",
+  AWAY: "Away",
+  DO_NOT_DISTURB: "Do_Not_Disturb",
+  IN_MEETING: "In_Meeting",
+  PRESENTING: "Presenting",
+  IN_CALENDAR_EVENT: "In_Calendar_Event",
+  ON_PHONE_CALL: "On_Phone_Call",
+  OFFLINE: "Offline",
+};
+
+const userPresenceUpdatedExample = {
+  event: "user.presence_status_updated",
+  event_ts: 1668633833110,
+  payload: {
+    account_id: "bJX4J5aWQxuPho8pYPqc5Q",
+    object: {
+      date_time: "2022-11-16T21:23:53Z",
+      email: "ji@launchpadlab.com",
+      id: "6_6i90scqy2rdcud_m17pa",
+      presence_status: "In_Meeting",
+    },
+  },
+};
+
+const meetingParticipantJoinedExample = {
+  event: "meeting.participant_joined",
+  event_ts: 1668633538589,
+  payload: {
+    account_id: "bJX4J5aWQxuPho8pYPqc5Q",
+    object: {
+      uuid: "RxXsefvASOujYR4yX9RmYw==",
+      participant: {
+        user_id: "16778240",
+        user_name: "Ji Park",
+        participant_user_id: "6_6I90sCQY2RdcuD_m17PA",
+        id: "6_6I90sCQY2RdcuD_m17PA",
+        join_time: "2022-11-16T21:19:56Z",
+        email: "ji@launchpadlab.com",
+      },
+      id: "86191416733",
+      type: 1,
+      topic: "Ji Park's Zoom Meeting",
+      host_id: "6_6I90sCQY2RdcuD_m17PA",
+      duration: 808591414,
+      start_time: "2022-11-16T21:18:55Z",
+      timezone: "",
+    },
+  },
+};
+
+const meetingParticipantLeftExample = {
+  event: "meeting.participant_left",
+  event_ts: 1668633729526,
+  payload: {
+    account_id: "bJX4J5aWQxuPho8pYPqc5Q",
+    object: {
+      uuid: "/fn5XvEnQAiSCcJa36Dhfw==",
+      participant: {
+        leave_time: "2022-11-16T21:22:06Z",
+        user_id: "16879616",
+        user_name: "Ji Park",
+        registrant_id: "",
+        participant_user_id: "6_6I90sCQY2RdcuD_m17PA",
+        id: "6_6I90sCQY2RdcuD_m17PA",
+        leave_reason: "left the meeting. Reason : left the meeting",
+        email: "ji@launchpadlab.com",
+      },
+      id: "83465221546",
+      type: 2,
+      topic: "Zoom Meeting",
+      host_id: "PHC6HC-FSYexKI10QEfdfg",
+      duration: 60,
+      start_time: "2022-11-16T18:02:21Z",
+      timezone: "America/Chicago",
+    },
+  },
+};
+
+module.exports = { SlackEventType, SlackUserPresenceStatus };
